bbcb7c1261f37d5133ede159bdb91c3745eb69ff
achiewe <mamukashviliachi706@gmail.com>
2024-03-05 20:38:46 +0400

    add the logic that if arithmetic size equal to 1 then return arithmetic else if geo size equal to 1 return geo

--- script.js
+++ script.js
@@ -23,9 +23,15 @@ function mathSequences(arr) {
     let number2 = arr[i] / arr[i - 1];
     geo.add(number2);
   }
-  console.log(arithmetic);
-  console.log(geo);
-  // return arr;
+
+  if (arithmetic.size === 1) {
+    return "Arithmetic";
+  }
+  if (geo.size === 1) {
+    return "geo";
+  }
+
+  return -1;
 }
 
 console.log(mathSequences([2, 4, 6, 8]));
