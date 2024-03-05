ef2e70c30ebdaff64c12b89c848da14f4a269e45
achiewe <mamukashviliachi706@gmail.com>
2024-03-05 20:21:54 +0400

    do some math suqeunces

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function longestWord(str) {
 }
 
 console.log(longestWord("vaa achi ravaxr"));
+
+function mathSequences(arr) {
+  let arithmetic = [];
+  let geo = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    let number1 = arr[i] - arr[i - 1];
+    arithmetic.push(number1);
+    let number2 = arr[i] / arr[i - 1];
+    geo.push(number2);
+  }
+  console.log(`A : ${arithmetic}`);
+  console.log(`G : ${geo}`);
+  // return arr;
+}
+
+console.log(mathSequences([2, 4, 6, 8]));
