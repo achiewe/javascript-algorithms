ea468dc6f91aa116bb18fb802ed020e9a7e715e7
achiewe <mamukashviliachi706@gmail.com>
2024-03-05 20:58:37 +0400

    add the countring leters logic

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// take longest word
+
 function longestWord(str) {
   let words = str.split(" ");
   let longestWord = "";
@@ -13,6 +15,7 @@ function longestWord(str) {
 
 console.log(longestWord("vaa achi ravaxr"));
 
+// math sequences
 function mathSequences(arr) {
   let arithmetic = new Set();
   let geo = new Set();
@@ -34,4 +37,38 @@ function mathSequences(arr) {
   return -1;
 }
 
-console.log(mathSequences([10, 20, 30, 40]));
+// console.log(mathSequences([10, 20, 30, 40]));
+
+// counting letters
+function countLetter(str) {
+  let tempArr = str.split(" ");
+  temp = tempArr.map((item) => {
+    let tempItem = item.toLowerCase().split("");
+    return tempItem.reduce(
+      (acc, current) => {
+        acc[current] = acc[current] ? acc[current] + 1 : 1;
+        if (acc[current] > acc.max) {
+          acc.max = acc[current];
+        }
+        return acc;
+      },
+      { max: 1, word: item }
+    );
+  });
+
+  let amount = 1;
+  let word = "";
+  for (let item of tempArr) {
+    if (item.max > amount) {
+      amount = item.max;
+      word = item.word;
+    }
+  }
+
+  if (amount > 1) {
+    return word;
+  }
+  return -1;
+}
+
+console.log(countLetter("whats up guys of all time 1121"));
