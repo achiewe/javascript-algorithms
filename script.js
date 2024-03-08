4c5bc2a1851fc819980a7fb5912d0aa5d4d275ed
achiewe <mamukashviliachi706@gmail.com>
2024-03-08 19:43:10 +0400

    add the logic without if statemnt and with ternary operator

--- script.js
+++ script.js
@@ -285,14 +285,8 @@ const dailyTotal = staff.reduce((total, person) => {
 // console.log(dailyTotal);
 
 function sumAll(arr) {
-  let min, max;
-  if (arr[0] > arr[1]) {
-    max = arr[0];
-    min = arr[1];
-  } else {
-    max = arr[1];
-    min = arr[0];
-  }
+  const min = arr[0] < arr[1] ? arr[0] : arr[1];
+  const max = arr[0] > arr[1] ? arr[1] : arr[0];
 
   let sum = 0;
   for (let i = min; i <= max; i++) {
@@ -302,4 +296,4 @@ function sumAll(arr) {
   return sum;
 }
 
-console.log(sumAll([1, 4]));
+console.log(sumAll([4, 4]));
