32fec373626c335fd66272241c9568ddd97f0bca
achiewe <mamukashviliachi706@gmail.com>
2024-03-08 12:08:25 +0400

    which one is prime number that will return false and ifis not prime number then will return true

--- script.js
+++ script.js
@@ -202,8 +202,14 @@ function isPrime(num) {
   if (num < 2) {
     return false;
   }
+  // modulus %
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
 
-  return num;
+  return true;
 }
 
 console.log(isPrime(8));
