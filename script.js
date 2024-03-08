32d74b201001b3cca057cbae35938f56b198dca6
achiewe <mamukashviliachi706@gmail.com>
2024-03-08 11:43:35 +0400

    create function where string will contain numbers or symbols its contain only letters

--- script.js
+++ script.js
@@ -188,9 +188,16 @@ function testRepeat(arr) {
   return value;
 }
 
-console.log(testRepeat([5, 2, 2, 1, 5]));
-//return 5
-console.log(testRepeat([6, 5, 5, 10, 10, 10]));
-//return 10;
-console.log(testRepeat([3, 4, 1, 6, 10]));
-//return -1;
+// console.log(testRepeat([5, 2, 2, 1, 5]));
+// //return 5
+// console.log(testRepeat([6, 5, 5, 10, 10, 10]));
+// //return 10;
+// console.log(testRepeat([3, 4, 1, 6, 10]));
+// //return -1;
+
+// string will not contain numbers or symbols only letter
+function countLetters(str) {
+  return str;
+}
+
+console.log(countLetters("fffeeerrrtttttooooo"));
