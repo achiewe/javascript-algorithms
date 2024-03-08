6cc3c88f615ec1a3d07b45bd2c495c3aa8c579d9
achiewe <mamukashviliachi706@gmail.com>
2024-03-08 19:47:32 +0400

    we can use also instead of ternary operator in the for loop math.min max math max methods to take from the array and it will work same

--- script.js
+++ script.js
@@ -285,11 +285,8 @@ const dailyTotal = staff.reduce((total, person) => {
 // console.log(dailyTotal);
 
 function sumAll(arr) {
-  const min = arr[0] < arr[1] ? arr[0] : arr[1];
-  const max = arr[0] > arr[1] ? arr[1] : arr[0];
-
   let sum = 0;
-  for (let i = min; i <= max; i++) {
+  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
     sum += i;
   }
 
