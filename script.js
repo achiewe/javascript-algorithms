d2eba4249c94a09f3ab0ed76f6a49a36dfcc3080
achiewe <mamukashviliachi706@gmail.com>
2024-03-08 19:40:38 +0400

    pass an array two numbers and return the sum of those two numbers plus sum of all the numbers between then

--- script.js
+++ script.js
@@ -282,4 +282,24 @@ const dailyTotal = staff.reduce((total, person) => {
   return total;
 }, 0);
 
-console.log(dailyTotal);
+// console.log(dailyTotal);
+
+function sumAll(arr) {
+  let min, max;
+  if (arr[0] > arr[1]) {
+    max = arr[0];
+    min = arr[1];
+  } else {
+    max = arr[1];
+    min = arr[0];
+  }
+
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+console.log(sumAll([1, 4]));
