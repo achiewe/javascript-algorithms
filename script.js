98e55a1e76b7c17333084b1b98c2fbabc8395ff2
achiewe <mamukashviliachi706@gmail.com>
2024-03-09 16:05:26 +0400

    create function to find the smallest common multiple of the provided marameters that can be evenly divided by both all sequentlal numbers in the range between these parameters

--- script.js
+++ script.js
@@ -310,4 +310,10 @@ function diffArray(arr1, arr2) {
   return symDiff;
 }
 
-console.log(diffArray([1, 2, 3, 5, 6], [1, 2, 3, 4, 5]));
+// console.log(diffArray([1, 2, 3, 5, 6], [1, 2, 3, 4, 5]));
+
+function smallestCommons(arr) {
+  return arr;
+}
+
+smallestCommons([1, 5]);
