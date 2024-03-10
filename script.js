91b475778e2697b70f19432b96910a53ec9c024e
achiewe <mamukashviliachi706@gmail.com>
2024-03-10 14:58:54 +0400

    we can flip them like that too

--- script.js
+++ script.js
@@ -388,9 +388,7 @@ function bubbleSort(arr) {
   for (i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
-        const temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        [[arr[j]], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }
