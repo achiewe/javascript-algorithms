d8dbb48bd87751bdd1166151c14c9e0dab9ed092
achiewe <mamukashviliachi706@gmail.com>
2024-03-11 17:26:43 +0400

    delete helper function

--- script.js
+++ script.js
@@ -556,20 +556,22 @@ var chunk = function (arr, size) {
   }
 };
 
-function objDiff(obj1, obj2) {
-  function helper(o1, o2) {
-    if (!isObject(o1) && !isObject(o2)) {
-      return o1 === o2 ? {} : [o1, o2];
-    }
-
-    if (!isObject(o1) || !isObject(o2)) {
-      return [o1, o2];
-    }
-
-    if (Array.isArray(o1) !== Array.isArray(o2)) {
-      return [o1, o2];
-    }
+function objDiff(o1, o2) {
+  if (!isObject(o1) && !isObject(o2)) {
+    return o1 === o2 ? {} : [o1, o2];
   }
+
+  if (!isObject(o1) || !isObject(o2)) {
+    return [o1, o2];
+  }
+
+  if (Array.isArray(o1) !== Array.isArray(o2)) {
+    return [o1, o2];
+  }
+
+  const diff = {};
+
+  return diff;
 
   function isObject(obj) {
     return typeof obj === "object" && obj !== null;
