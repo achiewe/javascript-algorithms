e3c6123a13fe981e2dfb6a1f293929acb67f61dc
achiewe <mamukashviliachi706@gmail.com>
2024-03-13 15:45:46 +0400

    covert it to lowercase

--- script.js
+++ script.js
@@ -711,6 +711,8 @@ function spinalCase(str) {
   str = str.replace(/_/g, "-").replace(/\s+/g, "-");
   str = str.replace(/([a-z])([A-Z])/g, "$1-$2");
 
+  str = str.toLowerCase();
+
   return str;
 }
 
