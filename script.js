5fa2ea9a9bd4f7e5801343275911893634b178a4
achiewe <mamukashviliachi706@gmail.com>
2024-03-18 11:58:18 +0400

    write an epmty array to store the result wave and then write for loop to through each character of the input string and checked if the haracter is whitespace and maintaining the original character positions

--- script.js
+++ script.js
@@ -1174,5 +1174,13 @@ function luckCheck(ticket) {
 
 // funtion wave
 function wave(str) {
+  const result = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== " ") {
+      result.push(str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1));
+    }
+  }
   return result;
 }
+
+console.log(wave("hello"));
