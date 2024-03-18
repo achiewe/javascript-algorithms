5198bc28e7e0de341fd7882aa1e17c7e6afb17c1
achiewe <mamukashviliachi706@gmail.com>
2024-03-18 17:15:53 +0400

    write the function to check if a number is prime

--- script.js
+++ script.js
@@ -1299,3 +1299,16 @@ function goldbachPartitions(n) {
 
   return result;
 }
+
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+
+  return true;
+}
