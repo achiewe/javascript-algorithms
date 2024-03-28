aac7bfee09aadbe5729c19830521696aafdb44e3
achiewe <mamukashviliachi706@gmail.com>
2024-03-28 15:55:59 +0400

    write console.logs to see the result

--- script.js
+++ script.js
@@ -1635,8 +1635,35 @@ function minPrice(arr) {
   return maxReachable + 1;
 }
 
-const coins1 = [3, 5];
-console.log(minPrice(coins1)); // Output: 8
+// const coins1 = [3, 5];
+// console.log(minPrice(coins1)); // Output: 8
 
-const coins2 = [7, 9];
-console.log(minPrice(coins2)); // Output: -1
+// const coins2 = [7, 9];
+// console.log(minPrice(coins2)); // Output: -1
+
+function balancedParens(n) {
+  const result = [];
+
+  function generate(current, open, close) {
+    if (current.length === n * 2) {
+      result.push(current);
+      return;
+    }
+
+    if (open < n) {
+      generate(current + "(", open + 1, close);
+    }
+
+    if (close < open) {
+      generate(current + ")", open, close + 1);
+    }
+  }
+
+  generate("", 0, 0);
+  return result;
+}
+
+console.log(balancedParens(0)); // Output: [""]
+console.log(balancedParens(1)); // Output: ["()"]
+console.log(balancedParens(2)); // Output: ["()()", "(())"]
+console.log(balancedParens(3)); // Output: ["()()()", "(())()", "()(())", "(()())", "((()))"]
