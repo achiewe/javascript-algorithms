e3ad4ec10dcfec334aa4bc3ff23ad08492741290
achiewe <mamukashviliachi706@gmail.com>
2024-04-20 20:22:23 +0400

    array of object containing the value and corresponding roman numeral symbols

--- script.js
+++ script.js
@@ -1678,4 +1678,20 @@ function isItLetter(char) {
 // console.log(isItLetter("5"));
 // console.log(isItLetter("@"));
 
-function intToRoman(num) {}
+function intToRoman(num) {
+  const romanNumerals = [
+    { value: 1000, symbol: "M" },
+    { value: 900, symbol: "CM" },
+    { value: 500, symbol: "D" },
+    { value: 400, symbol: "CD" },
+    { value: 100, symbol: "C" },
+    { value: 90, symbol: "XC" },
+    { value: 50, symbol: "L" },
+    { value: 40, symbol: "XL" },
+    { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
+    { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
+    { value: 1, symbol: "I" },
+  ];
+}
