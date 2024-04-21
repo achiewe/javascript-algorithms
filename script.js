2a105ecdc4ee72ad871fa9c6da817645691c7ca1
achiewe <mamukashviliachi706@gmail.com>
2024-04-21 22:09:42 +0400

    check if the year is divisible by 4 and not divisible by 100

--- script.js
+++ script.js
@@ -1705,4 +1705,10 @@ function removeExclamationMarks(s) {
   return s.replace(/!/g, "");
 }
 
-function yearDays(year) {}
+function yearDays(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return year + " has 366 days";
+  } else {
+    return year + " has 365 days";
+  }
+}
