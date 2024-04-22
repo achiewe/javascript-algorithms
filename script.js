39fa402f3fb41931f90502ce685c90d63037dbae
achiewe <mamukashviliachi706@gmail.com>
2024-04-22 13:01:33 +0400

    create function to allows the pirate captain to efficiently determine wheather its safe to fire the cannons

--- script.js
+++ script.js
@@ -1769,3 +1769,5 @@ function arrayPlusArray(arr1, arr2) {
 
 console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]));
 console.log(arrayPlusArray([-10, 5, 20], [30, -15]));
+
+const cannonsReady = (gunners) => {};
