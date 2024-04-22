a3b18983246436a0258cc7a81e3ce078b604dd41
achiewe <mamukashviliachi706@gmail.com>
2024-04-22 14:33:01 +0400

    check the input against the provided professions and return the corresponding drink

--- script.js
+++ script.js
@@ -1814,4 +1814,21 @@ console.log(feast("tiger", "tuna salad"));
 
 function getDrinkByProfession(param) {
   const profession = param.toLowerCase();
+
+  switch (profession) {
+    case "jabroni":
+      return "Patron Tequila";
+    case "school counselor":
+      return "Anything with Alcohol";
+    case "programmer":
+      return "Hipster Craft Beer";
+    case "bike gang member":
+      return "Moonshine";
+    case "politician":
+      return "Your tax dollars";
+    case "rapper":
+      return "Cristal";
+    default:
+      return "Beer";
+  }
 }
