fc3ecd63a8d532ddf7071fe9cb245289dcd87d57
achiewe <mamukashviliachi706@gmail.com>
2024-04-22 12:30:25 +0400

    create variable to store the sum and write for loop through the elements of the first array and add them to the sum

--- script.js
+++ script.js
@@ -1755,5 +1755,8 @@ console.log(monkeyCount(10));
 console.log(monkeyCount(1));
 
 function arrayPlusArray(arr1, arr2) {
-  return arr1 + arr2;
+  let sum = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    sum += arr1[i];
+  }
 }
