ce1eff6e9d85e5b7ffa0892e1049d415dd084dc7
achiewe <mamukashviliachi706@gmail.com>
2024-04-22 12:34:49 +0400

    create second loop throug the elements of the second array and add them to the sum

--- script.js
+++ script.js
@@ -1759,4 +1759,8 @@ function arrayPlusArray(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     sum += arr1[i];
   }
+
+  for (let i = 0; i < arr2.length; i++) {
+    sum += arr2[i];
+  }
 }
