1675701c99b4b9c3b542235d5b9c6b959a4e7f89
achiewe <mamukashviliachi706@gmail.com>
2024-04-23 17:07:57 +0400

    check the number of dogs and selectiong the appropriate response

--- script.js
+++ script.js
@@ -1878,10 +1878,20 @@ function leo(oscar) {
 }
 
 function howManyDalmations(number) {
-  var dogs = [
+  let dogs = [
     "Hardly any",
     "More than a handful!",
     "Woah that's a lot of dogs!",
     "101 DALMATIONS!!!",
   ];
+  let respond =
+    number <= 10
+      ? dogs[0]
+      : number <= 50
+      ? dogs[1]
+      : number === 101
+      ? dogs[3]
+      : dogs[2];
+
+      return respond;
 }
