a8513b51e2b474b9ae1bc32badcf48eb824dc030
achiewe <mamukashviliachi706@gmail.com>
2024-04-27 18:05:24 +0400

    create for loop through the array and write foeach element check if arr + n exists in the array if it does will be increment count and return the final count

--- script.js
+++ script.js
@@ -2006,4 +2006,14 @@ function evenOrOdd(number) {
 const intDiff = (arr, n) => {
   arr.sort((a, b) => a - b);
   let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.includes(arr[i] + n)) {
+      count++;
+    }
+  }
+
+  return count;
 };
+
+console.log(intDiff([1, 1, 5, 6, 9, 16, 27], 4));
+console.log(intDiff([1, 1, 3, 3], 2));
