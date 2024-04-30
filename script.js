fccd7f31a893be93c3945a1d7ed6d6413a529808
achiewe <mamukashviliachi706@gmail.com>
2024-04-30 11:31:47 +0400

    define the layout of the mobile keyboard

--- script.js
+++ script.js
@@ -2069,4 +2069,45 @@ function pak(s) {
   }
 }
 
-function mobileKeyboard(str) {}
+function mobileKeyboard(str) {
+  const keyboardLayout = {
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    0: 0,
+    "*": 1,
+    "#": 1,
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 1,
+    e: 2,
+    f: 3,
+    g: 1,
+    h: 2,
+    i: 3,
+    j: 1,
+    k: 2,
+    l: 3,
+    m: 1,
+    n: 2,
+    o: 3,
+    p: 1,
+    q: 2,
+    r: 3,
+    s: 4,
+    t: 1,
+    u: 2,
+    v: 3,
+    w: 1,
+    x: 2,
+    y: 3,
+    z: 4,
+  };
+}
