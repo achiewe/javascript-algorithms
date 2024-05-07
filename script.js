00d3bb58a733fff3f20be5a9431f25f0766eaf12
achiewe <mamukashviliachi706@gmail.com>
2024-05-07 17:43:54 +0400

    add the deep copy example

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let originalArray = [1, 2, 3, 4];
 let copiedArray = originalArray;
 copiedArray[0] = 5;
 console.log(originalArray);
+
+// deep copy
+let deeporiginal = [1, 2, 3, 4];
+let deepCopy = [...deeporiginal];
+deepCopy[0] = 8;
+console.log(deeporiginal);
