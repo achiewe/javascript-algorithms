9ced2bd49bcfce5d74aa3472d8e9947b83f9676a
achiewe <mamukashviliachi706@gmail.com>
2024-05-12 19:57:26 +0400

    and then add helper function to get power of a letter

--- script.js
+++ script.js
@@ -156,3 +156,10 @@ function trenchAssault(s) {
   }
   return frontline;
 }
+
+function getPower(letter) {
+  const leftSidePowers = { w: 4, p: 3, b: 2, s: 1 };
+  const rightSidePowers = { m: 4, q: 3, d: 2, z: 1 };
+
+  return leftSidePowers[letter] || rightSidePowers[letter] || 0;
+}
