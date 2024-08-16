93a14d5f75e7873ccd21e95fbde488cc12517cdb
achiewe <mamukashviliachi706@gmail.com>
2024-08-16 20:12:51 +0400

    use reduce method to accumulate the single value from an array

--- script.js
+++ script.js
@@ -91,3 +91,10 @@ const ninjaWithKatana = soroi.find(soroi => {
   assert(armedNinjas[0].name === "Yagyu" 
   && armedNinjas[1].name === "Kuma", "Yagyu and Kuma"); 
 
+
+
+  const numbers = [1, 2, 3, 4]; 
+  const sum = numbers.reduce((aggregated, number) => 
+  aggregated + number, 0); 
+  assert(sum === 10, "The sum of first four numbers is 10")
+
