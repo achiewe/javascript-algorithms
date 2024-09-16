d2be5e37ba3899a446e0396af08a558168214c24
achiewe <mamukashviliachi706@gmail.com>
2024-09-16 16:43:20 +0400

    use join method and see the result in the console

--- script.js
+++ script.js
@@ -159,3 +159,5 @@ let capitalizedWords = [];
 for (let i = 0; i < words.length; i++) {
   capitalizedWords.push(words[i][0].toUpperCase() + words[i].slice(1));
 }
+const result = capitalizedWords.join(" ");
+console.log(result); 
