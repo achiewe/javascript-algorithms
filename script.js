cbd81e62c6c1b4d4412cfee066514acc2ee72921
achiewe <mamukashviliachi706@gmail.com>
2024-10-04 19:54:12 +0400

    initialize time ariables parse initial time into hours and minutes

--- script.js
+++ script.js
@@ -9,4 +9,9 @@ function vaporwave(sentence) {
 console.log(vaporwave("Lets go to the movies")); 
 console.log(vaporwave("Why isn't my code working?"));
 
-function cuckooTime(initial_time, n) {}
+function cuckooTime(initial_time, n) {
+    const [initialHours, initialMinutes] = initial_time.split(':').map(Number);
+    let totalChimes = 0;
+    let currentHours = initialHours;
+    let currentMinutes = initialMinutes;
+}
