f5c2e652840b95ec5e662b0c8bd9576a3e906ad8
achiewe <mamukashviliachi706@gmail.com>
2024-10-16 23:59:15 +0400

    delete function from the code

--- script.js
+++ script.js
@@ -9,30 +9,6 @@ function vaporwave(sentence) {
 console.log(vaporwave("Lets go to the movies")); 
 console.log(vaporwave("Why isn't my code working?"));
 
-function cuckooTime(initial_time, n) {
-    const [initialHours, initialMinutes] = initial_time.split(':').map(Number);
-    let totalChimes = 0;
-    let currentHours = initialHours;
-    let currentMinutes = initialMinutes;
-}
 
-function findParetoFront(points) {
-    let paretoFront = new Set();
 
-    points.forEach((point, index) => {
-        let isDominated = false;
-
-        points.forEach((otherPoint, otherIndex) => {
-            if (index !== otherIndex && dominates(otherPoint, point)) {
-                isDominated = true;
-            }
-        });
-
-        if (!isDominated) {
-            paretoFront.add(point);
-        }
-    });
-
-    return paretoFront;
-}
 
