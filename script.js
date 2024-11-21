5900f02fc67460551a4162ebde0b6965343de4c6
achiewe <mamukashviliachi706@gmail.com>
2024-11-21 19:07:18 +0400

    implement function to generate all prime numbers up to a given maximum

--- script.js
+++ script.js
@@ -71,4 +71,16 @@ console.log(areAnagrams("hello", "world"));
 
 function getPrimes(max) {
   if (max < 2) return []; 
+  const primes = [];
+    for (let num = 2; num <= max; num++) {
+        let isPrime = true;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) primes.push(num);
+    }
+    return primes;
 }
