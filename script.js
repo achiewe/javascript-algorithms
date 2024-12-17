ef761b76a59110109ecb38f69fd3bf71797b5664
achiewe <mamukashviliachi706@gmail.com>
2024-12-17 16:26:20 +0400

    buildOneTwoThree function creates a list start with an empty list add 3 to the list add 2 to the list add 1 to the list

--- script.js
+++ script.js
@@ -181,3 +181,11 @@ function push(head, data) {
   newNode.next = head;
   return newNode;      
 }
+
+function buildOneTwoThree() {
+  let head = null;          
+  head = push(head, 3);     
+  head = push(head, 2);     
+  head = push(head, 1);      
+  return head;               
+}
