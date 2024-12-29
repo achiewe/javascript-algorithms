3f6808c95511561cacdca6658c1075be008752fd
achiewe <mamukashviliachi706@gmail.com>
2024-12-29 17:09:39 +0400

    filll the DP table interatively item can be include choose max of including or excluding

--- script.js
+++ script.js
@@ -275,4 +275,19 @@ function knapsack(scores, weights, capacity) {
   const n = scores.length;
 
   const dp = Array.from({ length: n + 1 }, () => Array(capacity + 1).fill(0));
+  for (let i = 1; i <= n; i++) {
+    for (let w = 1; w <= capacity; w++) {
+      if (weights[i - 1] <= w) {
+        dp[i][w] = Math.max(
+          scores[i - 1] + dp[i - 1][w - weights[i - 1]], 
+          dp[i - 1][w] 
+        );
+      } else {
+        dp[i][w] = dp[i - 1][w];
+      }
+    }
+  }
+
+  return dp[n][capacity];
+
 }
